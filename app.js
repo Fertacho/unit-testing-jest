9904855d10dd7b1194fb04db0dffcf0c7333568c
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-05-25 20:44:29 +0000

    avance

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+// we declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
+}
+
+const 
+
 const sum = (a,b) => {
     return a + b
 }
 
 console.log(sum(7,3))
 
-module.exports = {sum}
+module.exports = {sum, fromEuroToDollar}
--- test.js
+++ test.js
@@ -1,8 +1,23 @@
 const {sum} = require ('./app.js');
 
-test ('adds 14 +9 to equal 23',() => {
+test ('adds 14 + 9 to equal 23',() => {
 
     let total = sum(14,9);
 
     expect(total).toBe(23)
 });
+
+
+test("One euro should be 1.206 dollars", function(){
+    //import the function from app.js
+    const { fromEuroToDollar } = require('./app.js')
+
+    // use the function like its suppoed to be used
+    const dollars = fromEuroToDollar(3.5)
+
+    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
+    const expected = 3.5 * 1.2; 
+    
+    // this is the comparison for the unit test
+     expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+})
